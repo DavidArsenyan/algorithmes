7b4f1ed46cb147825e58ad6e6fa11e61f0ee36e1
David Arsenyan <david.arsenyan.2017@gmail.com>
2024-12-22 03:23:58 +0400

    First commit

---
+++ ConsoleApp4/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//  Lognest common subsequence
+
+namespace Class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string B = "algorithm";
+            string A = "rhythm";
+            //find LCS in A,B starting from index 0 of each
+            int longestCommonSubsequence = LCS(A, B, 0, 0);
+            Console.WriteLine(longestCommonSubsequence);
+            Console.Read();
+
+        }
+
+        //Find the longest common subsequnce starting from index1 in A and index2 in B
+        //Pass A as shorter string
+        public static int LCS(String A, String B, int index1, int index2)
+        {
+            int max = 0;
+            if (index1 == A.Length)
+            {
+                //You have reached beyond A and thus no subsequence
+                return 0;
+            }
+            if (index2 == B.Length)
+            {   //you may reach end of 2nd string. LCS from that end is 0
+                return 0;
+            }
+
+            for (int i = index1; i < A.Length; i++)
+            {
+                int exist = B.IndexOf(A[i], index2);
+                if (exist != -1)
+                {
+                    //   found = true;
+
+                    int temp = 1 + LCS(A, B, i + 1, exist + 1);
+                    if (max < temp)
+                    {
+                        max = temp;
+                    }
+
+
+                }
+
+
+            }
+            return max;
+
+        }
+    }
+}
---
+++ ConsoleApp4/part_3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class CustomList<T>
+{
+    private T[] _items;
+    private int _count;
+    private const int _initialCapacity = 4;
+
+    public CustomList()
+    {
+        _items = new T[_initialCapacity];
+        _count = 0;
+    }
+
+    public int Count => _count;
+
+    public void Add(T item)
+    {
+        if (_count == _items.Length)
+        {
+            Resize();
+        }
+        _items[_count++] = item;
+    }
+
+    public T Get(int index)
+    {
+        if (index < 0 || index >= _count)
+        {
+            throw new IndexOutOfRangeException("Index out of range.");
+        }
+        return _items[index];
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= _count)
+        {
+            throw new IndexOutOfRangeException("Index out of range.");
+        }
+
+        for (int i = index; i < _count - 1; i++)
+        {
+            _items[i] = _items[i + 1];
+        }
+        _items[--_count] = default; // Clear the last item
+    }
+
+    public void Clear()
+    {
+        _items = new T[_initialCapacity];
+        _count = 0;
+    }
+
+    private void Resize()
+    {
+        int newCapacity = _items.Length * 2;
+        T[] newItems = new T[newCapacity];
+        Array.Copy(_items, newItems, _count);
+        _items = newItems;
+    }
+
+    public T this[int index]
+    {
+        get => Get(index);
+        set
+        {
+            if (index < 0 || index >= _count)
+            {
+                throw new IndexOutOfRangeException("Index out of range.");
+            }
+            _items[index] = value;
+        }
+    }
+}
